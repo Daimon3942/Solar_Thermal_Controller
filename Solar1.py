385b656e6cceb5f1243ca819dfdfcb9d8673bc3d
Daimon3942 <daimon@simplyeff.com>
2014-02-05 08:30:59 -0700

    Notes for building program

---
+++ Solar1.py
@@ -0,0 +1,9 @@
+#need to add the top line for all programs
+#This program will run a closed loop SDHW system with a second pump to
+#recirc. from SST to DHW htr.
+#in addition, this program will allow for additional sensors on the SST
+#to determine how much stratification is taking place
+#this system should also be able to add crude CT's used to tell if the
+#pumps are running and if a elec. water heater is running.
+#
+#what mod's are needed?
